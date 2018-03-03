bb8574f3ed2a48790c31e95d9e5cc58b35bc70de
David Leung <dleung@ca.ibm.com>
2018-03-02 21:37:13 -0500

    adds href

--- src/App.js
+++ src/App.js
@@ -179,7 +179,7 @@ class App extends Component {
           </div> :
           <button
             onClick={() => {
-              window.location = window.location.includes('localhost')
+              window.location = window.location.href.includes('localhost')
                 ? 'http://localhost:8888/login'
                 : 'https:  //spot-playlists-backend.herokuapp.com/login';
             }}
